a372e6005fa59331be32d2720c5ff96a3a2a2a75
minq <james2224@hanmail.net>
2024-07-05 10:02:04 +0900

    Changes for mission1

--- Target/DeviceDriver.cpp
+++ Target/DeviceDriver.cpp
@@ -1,3 +1,5 @@
+
+
 #include "DeviceDriver.h"
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
@@ -5,12 +7,22 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    unsigned char nReadValue = m_hardware->read(address);
+    for (int nRetry = 1; nRetry < READ_RETRY_COUNT; nRetry++)
+    {
+        if (nReadValue != m_hardware->read(address))
+            throw ReadFailException();
+    }
+
+    return (int)nReadValue;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    unsigned char nPreReadValue = m_hardware->read(address);
+
+    if (nPreReadValue != DATA_VALUE_ERASED)
+        throw WriteFailException();
+
     m_hardware->write(address, (unsigned char)data);
 }
--- TestEnv/TestEnv.cpp
+++ TestEnv/TestEnv.cpp
@@ -1,7 +1,73 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Target/DeviceDriver.cpp"
+#include "MockFlashMemoryDevice.h"
 
-TEST(TS000, TC000)
+using namespace testing;
+
+class DeviceDriverTestFixture : public testing::Test
 {
-    EXPECT_EQ(1, 1);
+protected:
+public:
+    NiceMock<MockFlashMemoryDevice> mockflash;
+    DeviceDriver devicedriver{ &mockflash };
+
+    static const int DATA_VALUE_ERASED = 0xFF;
+    static const int DATA_VALUE_FOR_TEST = 0xA5;
+
+    static const int DATA_ADDR_FOR_TEST = 0x1000;
+};
+
+TEST_F(DeviceDriverTestFixture, WriteAndReadNormal)
+{
+    // Arrange
+    EXPECT_CALL(mockflash, read(_))
+        .WillOnce(Return(DATA_VALUE_ERASED))
+        .WillRepeatedly(Return(DATA_VALUE_FOR_TEST));
+
+    // Act
+    devicedriver.write(DATA_ADDR_FOR_TEST, DATA_VALUE_FOR_TEST);
+    
+    // Assert
+    EXPECT_THAT(devicedriver.read(DATA_ADDR_FOR_TEST), Eq(DATA_VALUE_FOR_TEST));
+}
+
+TEST_F(DeviceDriverTestFixture, ReadErased)
+{
+    // Arrange
+    EXPECT_CALL(mockflash, read(_))
+        .WillRepeatedly(Return(DATA_VALUE_ERASED));
+
+    // Act
+    // Assert
+    EXPECT_THAT(devicedriver.read(DATA_ADDR_FOR_TEST), Eq(DATA_VALUE_ERASED));
+}
+
+TEST_F(DeviceDriverTestFixture, Overwrite)
+{
+    // Arrange
+    EXPECT_CALL(mockflash, read(_))
+        .WillOnce(Return(DATA_VALUE_ERASED))
+        .WillRepeatedly(Return(DATA_VALUE_FOR_TEST));
+
+    // Act
+    devicedriver.write(DATA_ADDR_FOR_TEST, DATA_VALUE_FOR_TEST);
+
+    // Assert
+    EXPECT_THROW(devicedriver.write(DATA_ADDR_FOR_TEST, DATA_VALUE_FOR_TEST), WriteFailException);
+}
+
+TEST_F(DeviceDriverTestFixture, ReadRetryFail)
+{
+    // Arrange
+    EXPECT_CALL(mockflash, read(_))
+        .WillOnce(Return(DATA_VALUE_ERASED))
+        .WillOnce(Return(DATA_VALUE_FOR_TEST + 1))
+        .WillRepeatedly(Return(DATA_VALUE_FOR_TEST));
+
+    // Act
+    devicedriver.write(DATA_ADDR_FOR_TEST, DATA_VALUE_FOR_TEST);
+
+    // Assert
+    EXPECT_THROW(devicedriver.read(DATA_ADDR_FOR_TEST), ReadFailException);
 }
